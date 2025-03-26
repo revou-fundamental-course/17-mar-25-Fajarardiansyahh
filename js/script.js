3412a88160c523f79aae3ef727c1ffaee9fbc9ac
RevoU-FajarArdiansyah <ardiansyahfajar9499@gmail.com>
2025-03-27 02:18:15 +0700

    update konfigurasi

--- js/script.js
+++ js/script.js
@@ -1,1 +1,87 @@
+const formPersegiPanjang=document.getElementById("form-persegi-panjang")
+const inputPersegiPanjang=document.getElementById("input-persegi-panjang")
+const hasilPersegiPanjang=document.getElementById("hasil-persegi-panjang")
+const inputLebar=document.getElementById("input-lebar")
+const formKelilingPersegi=document.getElementById("form-keliling-persegi")
+const inputKeliling=document.getElementById("input-keliling")
+const hasilKeliling=document.getElementById("hasil-keliling")
+const hasil=document.getElementById("hasil")
+const formLuasKeliling=document.getElementById("form-luas-keliling")
+const inputSisi=document.getElementById("input-sisi")
 
+function hitungpersegipanjang() {
+    const value=inputPersegiPanjang.value
+    const value2=inputLebar.value
+    if(!value && !value2){
+        alert("Input tidak boleh kosong")
+        return
+    }
+    const result=value*value2
+    hasilPersegiPanjang.innerText=(`
+        L=${value} x ${value2}
+        L=${value2} 
+        P=${value}
+        hasil=${result}
+        `)
+}
+
+function hitungluas() {
+    const value=inputSisi.value
+    if(!value){
+        alert("Input tidak boleh kosong")
+        return
+    }
+    const result=value*value
+    hasil.innerText=(`
+        L=${value} x ${value}
+        L=${value} 
+        S=${value}
+        hasil=${result}
+        `)
+}
+
+function hitungkeliling() {
+    const value=inputKeliling.value
+    if(!value){
+        alert("Input tidak boleh kosong")
+        return
+    }
+    const result=value*value
+    hasilKeliling.innerText=(`
+        L=${value} x ${value}
+        L=${value} 
+        S=${value}
+        hasil=${result}
+        `)
+}
+
+formLuasKeliling.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    hitungluas()
+})
+
+function reset() {
+    hasil.innerText=""
+    inputSisi.value=0
+}
+
+formKelilingPersegi.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    hitungkeliling()
+})
+
+function resetKeliling() {
+    hasilKeliling.innerText=""
+    inputKeliling.value=0
+}
+
+formPersegiPanjang.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    hitungpersegipanjang()
+})
+
+function resetPersegiPanjang() {
+    hasilPersegiPanjang.innerText=""
+    inputLebar.value=0
+    inputPersegiPanjang.value=0
+}
